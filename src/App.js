6056e57f9d000f3f49150cbe16b33a57b840651b
atif <ratul0947@gmail.com>
2022-01-07 15:47:11 +0600

    dev-tools initialized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useQuery } from "react-query";
 import axios from "axios";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
 	const queryInfo = useQuery("pokemon", async () => {
 		await new Promise((resolve) => setTimeout(resolve, 1000));
 
-		if (true) {
-			throw new Error("Something went wrong");
-		}
+		// if (true) {
+		// 	throw new Error("Something went wrong");
+		// }
 		return axios
 			.get("https://pokeapi.co/api/v2/pokemon")
 			.then((res) => res.data.results);
@@ -22,6 +23,7 @@ function App() {
 			{queryInfo.data.map((pokemon) => (
 				<div key={pokemon.name}>{pokemon.name}</div>
 			))}
+			<ReactQueryDevtools initialIsOpen={false} />
 		</div>
 	);
 }
