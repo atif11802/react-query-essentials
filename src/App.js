4267d11889568d896c885356c9964a716b27f1eb
atif <ratul0947@gmail.com>
2022-01-07 15:42:40 +0600

    error state done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import axios from "axios";
 function App() {
 	const queryInfo = useQuery("pokemon", async () => {
 		await new Promise((resolve) => setTimeout(resolve, 1000));
+
+		if (true) {
+			throw new Error("Something went wrong");
+		}
 		return axios
 			.get("https://pokeapi.co/api/v2/pokemon")
 			.then((res) => res.data.results);
@@ -11,6 +15,8 @@ function App() {
 
 	return queryInfo.isLoading ? (
 		"loading..."
+	) : queryInfo.isError ? (
+		queryInfo.error.message
 	) : (
 		<div className='App'>
 			{queryInfo.data.map((pokemon) => (
